068117c5472c24720abff6c3b190e43aa50929ef
nqg <nidaghuman@gmail.com>
2022-02-04 16:27:40 -0500

    added text + button to homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import 'animate.css';
+import React from "react";
+import "animate.css";
 import "./HomePageStyles.scss";
-import {Box, Image} from "@chakra-ui/react";
-import logo from "./../../assets/homepage-logo.png"
+
+import { useNavigate } from "react-router-dom";
+import { Box, Image, Center, Button } from "@chakra-ui/react";
+import logo from "./../../assets/homepage-logo.png";
 
 export const HomePage = () => {
-  return (
-  <div className='homepage-container'>
-    <Box className='logo-container animate__animated animate__fadeIn animate__slower'>
-    <Image src={logo}/>
-    </Box>
 
-  </div>
+  const navigate = useNavigate()
+  return (
+    <div className="homepage-container">
+      <Box className="logo-container animate__animated animate__fadeIn animate__slower">
+        <Image src={logo} />
+        <Box>
+          <Center className="homepage-text">
+            Simple and elegant To Do List for helping you get it done!
+            <Button
+              marginY={4}
+              colorScheme="pink"
+              onClick={() => {navigate("/todo")}}
+            >
+              Get Started
+            </Button>
+          </Center>
+        </Box>
+      </Box>
+    </div>
   );
 };
-
